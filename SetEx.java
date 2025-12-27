f8538f584ce17a9ffb81338032cdaaef3f2f8264
Vijayalaxmi45-ai <vijayalaxmikamble99@gmail.com>
2025-12-27 19:39:44 +0530

    Add files via upload

---
+++ SetEx.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class SetEx
+{
+	public static void main(String args[])
+	{
+		Set<Integer> s = new HashSet<>();
+		s.add(1);
+		s.add(4);
+		s.add(5);
+		s.add(30);
+		System.out.println(s);
+	}
+}
