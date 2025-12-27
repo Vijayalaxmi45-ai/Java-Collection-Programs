855de8fbf2291d4a638b5cbc31f169b14f211b24
Vijayalaxmi45-ai <vijayalaxmikamble99@gmail.com>
2025-12-27 19:33:30 +0530

    Add files via upload

---
+++ HashMapEx.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class HashMapEx
+{
+	public static void main(String args[])
+	{
+		Map<Integer,String> mp = new HashMap<>();
+		mp.put(1,"Vijaya");
+		mp.put(2,"Sneha");
+		mp.put(3,"Pallavi");
+		mp.put(4,"Payal");
+		System.out.println(mp);
+		
+	}
+}
