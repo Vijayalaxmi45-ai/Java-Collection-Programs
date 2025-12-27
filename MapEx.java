06019d4d94f7006c287f6616e5c78bcbb68922f2
Vijayalaxmi45-ai <vijayalaxmikamble99@gmail.com>
2025-12-27 19:30:44 +0530

    Add files via upload

---
+++ MapEx.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class MapEx
+{
+	public static void main(String args[])
+	{
+		Map<Integer,Integer> mp = new HashMap<>();
+		mp.put(1,20);
+		mp.put(30,2);
+		mp.put(4,31);
+		
+		System.out.println(mp);
+	}
+	
+}
